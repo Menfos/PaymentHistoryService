3063b56224877f73fc302ca3921f1fbb9ab589b1
Maksym Labish <maksym.labish@lebara.com>
2019-04-27 13:45:29 +0300

    Added dal library to solution. Added table entities.

---
+++ PaymentHistory.DAL/Entities/CustomerEntity.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PaymentHistory.DAL.Entities
+{
+    [Table("Customers")]
+    public class CustomerEntity
+    {
+        [Key]
+        [Column("CustomerId")]
+        public long CustomerId { get; set; }
+
+        [MaxLength(30)]
+        [Column("CustomerName")]
+        public string CustomerName { get; set; }
+
+        [MaxLength(25)]
+        [Column("Email")]
+        public string Email { get; set; }
+
+        [Column("MobileNumber")]
+        public long MobileNumber { get; set; }
+    }
+}
---
+++ PaymentHistory.DAL/Entities/TransactionEntity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PaymentHistory.DAL.Entities
+{
+    [Table("Transactions")]
+    public class TransactionEntity
+    {
+        [Key]
+        [Column("TransactionId")]
+        public long TransactionId { get; set; }
+
+        [Required]
+        [Column("TransactionDate")]
+        public DateTime TransactionDate { get; set; }
+
+        [Required]
+        [Column("Amount")]
+        public decimal Amount { get; set; }
+
+        [Required]
+        [MaxLength(3)]
+        [Column("CurrencyCode")]
+        public string CurrencyCode { get; set; }
+
+        [Required]
+        [MaxLength(15)]
+        [Column("Status")]
+        public string Status { get; set; }
+    }
+}
